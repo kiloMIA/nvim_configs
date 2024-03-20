82232215d6678ab2a6aed4d53d1f33f4ef95db39
kilo <pipianino@protonmail.com>
2024-03-20 19:12:32 +0500

    support for virtual env

--- plugins.lua
+++ plugins.lua
@@ -83,6 +83,22 @@ local plugins = {
       end
     end
   },
-
+  { "nvim-neotest/nvim-nio" },
+  {
+    'linux-cultist/venv-selector.nvim',
+    dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
+    opts = {
+      -- Your options go here
+      name = "venv",
+      search_workspace = true,
+    },
+    event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+    keys = {
+      -- Keymap to open VenvSelector to pick a venv.
+      { '<leader>vs', '<cmd>VenvSelect<cr>' },
+      -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+      { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
+    },
+  }
 }
 return plugins
